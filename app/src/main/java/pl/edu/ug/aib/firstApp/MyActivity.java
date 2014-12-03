f8d09d9bdc505f9ddf3342aec96b68cd09e19eb4
Daniel Dekanski <daniel.dekanski@goyello.com>
2014-12-03 20:08:25 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/ug/aib/firstApp/MyActivity.java
@@ -0,0 +1,37 @@
+package pl.edu.ug.aib.firstApp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_my)
+@OptionsMenu(R.menu.my)
+public class MyActivity extends ActionBarActivity {
+
+    @ViewById
+    public EditText username;
+
+    @ViewById
+    public EditText password;
+
+    @Click
+    void loginClicked()    {
+        if (username.getText().length() < 3) {
+            Toast.makeText(this, "Nazwa użytkownika musi się składać z przynajmniej 3 znaków", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SecondActivity_.intent(this).username(username.getText().toString()).start();
+    }
+
+    @OptionsItem
+    void settingsSelected() {
+    }
+
+}
---
+++ app/src/main/java/pl/edu/ug/aib/firstApp/SecondActivity.java
@@ -0,0 +1,42 @@
+package pl.edu.ug.aib.firstApp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterExtras;
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_second)
+public class SecondActivity extends ActionBarActivity {
+
+    @Extra
+    String username;
+
+    @ViewById
+    TextView welcomeText;
+
+    @ViewById
+    EditText phoneNumber;
+
+    @AfterViews
+    void init() {
+        welcomeText.setText("Cześć " + username + "!");
+    }
+
+    @Click
+    void dialClicked() {
+        String tel = phoneNumber.getText().toString();
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:" + tel));
+        startActivity(i);
+    }
+
+}
